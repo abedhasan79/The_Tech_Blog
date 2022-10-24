3d61de6c04238ab49a8b8a981bf119b2761aab3e
Abed Hasan <abedhasan79@gmail.com>
2022-10-24 18:46:51 -0400

    added utility files

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const PORT = process.env.PORT || 3001;
 
