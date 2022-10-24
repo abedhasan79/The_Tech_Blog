8758b95ccb0d208d5f6f2b1dd58c2abac66a8be6
Abed Hasan <abedhasan79@gmail.com>
2022-10-24 18:01:20 -0400

    set up user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+router.get('/login', async (req, res) => {
+
+});
+
+router.post('/logout', (req, res) => {
+
+});
+
+module.exports = router;
