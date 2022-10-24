b2caaf3964391115a38f35086dcbc927fb2648eb
Abed Hasan <abedhasan79@gmail.com>
2022-10-24 19:12:09 -0400

    updated user routes with a create a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+//login
 router.get('/login', async (req, res) => {
     try {
 
@@ -36,9 +58,10 @@ router.get('/login', async (req, res) => {
     }
 });
 
+//logout
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
-        
+
         req.session.destroy(() => {
             res.status(204).end();
         });
