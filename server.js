c67492e197941024d2d0b6821ebb1cf2f189e4e9
Abed Hasan <abedhasan79@gmail.com>
2022-10-24 19:21:54 -0400

    updated server.js with cookie

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ const app = express();
 //session
 const sess = {
     secret: process.env.SESSION_SECRET,
-    cookie: {},
+    cookie: {
+        //session will expire in one hour idel time
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
