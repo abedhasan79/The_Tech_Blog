4e9c9cc12edb3217bbf65614b896a590b659708c
Abed Hasan <abedhasan79@gmail.com>
2022-10-25 15:53:41 -0400

    added comment-routes.js

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { Comment, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+//Get comment
+router.get('/', async (req, res) => {
+    try {
+        const newComment = await Comment.findAll({
+            include:[User],
+        });
+
+        const comment = newComment.map((cmnt) => cmnt.get({plain: true}));
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//create a comment
+router.post('/',withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+
+module.exports = router;
